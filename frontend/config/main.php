8706cf0cef5481d06486ecc90cfc9a1ff92f3a5c
soroush <soroushshalali@gmail.com>
2020-10-03 13:02:08 +0200

    RESTfull(athentication)

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -36,23 +36,32 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        
+        // 'urlManager' => [
+        //     'enablePrettyUrl' => true,
+        //     // 'enableStrictParsing' => true,
+        //     'showScriptName' => false,
+        //     'rules' => [
+        //         ['class' => 'yii\rest\UrlRule', 'controller' => 'webservice'],
+
+        //     ],
+        // ],
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => array(
 
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
-        
+
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
-        
+
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
-        
-             ),
-        
+
+            ),
+
         ],
-        ],
-        
-    
+    ],
+
+
     'params' => $params,
 ];
--- frontend/controllers/WebServiceController.php
+++ frontend/controllers/WebServiceController.php
@@ -1,11 +1,38 @@
 <?php
 namespace frontend\controllers;
-use yii\rest\ActiveController;
 
-class WebServiceController extends ActiveController
+use Yii;
+use yii\filters\auth\HttpBasicAuth;
+use yii\rest\ActiveController;
+use common\models\User;
+
+class WebserviceController extends ActiveController
 {
 
     public $modelClass= 'frontend\models\ArticlesCrud';
+
+    public function init(){
+        parent::init();
+        Yii::$app->user->enableSession= false;
+    }
+
+    public function behaviors()
+    {
+        $behaviors=parent::behaviors();
+        $behaviors['athenticator']=[
+            'class' => HttpBasicAuth::className(),
+            'auth' => function($username , $password){
+                $user=User::findByUsername($username);
+                if ($user !== null){
+                    if ($user->validatePassword($password)){
+                    return $user; 
+                    }
+                }
+                return null;
+            }
+        ];
+        return $behaviors;
+    }
 
 }
 
